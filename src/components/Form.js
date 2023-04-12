982bdd0ae2a6af06a987911dcf3f2dc2f36dd158
Nody00 <dinokrcicprof@gmail.com>
2023-04-12 10:13:33 +0200

    aa

--- src/components/Form.js
+++ src/components/Form.js
@@ -9,38 +9,40 @@ const HeroForm = () => {
   }
   return (
     <div className={styles.formHeroSpecial}>
-      <div className={styles.smallContainer}>
-        <label className={styles.label}>Pick up date</label>
-        <input type="date" id="date" name="date" className={styles.input} />
-      </div>
-      <div className={styles.smallContainer}>
-        <label className={styles.label}>Drop off date</label>
-        <input className={styles.input} type="date" id="ddate" name="ddate" />
-      </div>
-      <div className={styles.smallContainer}>
-        <label className={styles.label}>Pick vehicle brand</label>
-        <select className={styles.input} name="cars" id="cars">
-          <option value="" key="1">
-            Pick a vehicle
-          </option>
-          <option value="" key="2">
-            Land rover
-          </option>
-          <option value="" key="3">
-            Jeep
-          </option>
-          <option value="" key="4">
-            Ford
-          </option>
-          <option value="" key="5">
-            Toyota
-          </option>
-        </select>
-      </div>
-      <div className={styles.smallContainer}>
-        <button onClick={handleSubmit} className={styles.button}>
-          Find vehicle
-        </button>
+      <div className={styles.formHeroSpecial2}>
+        <div className={styles.smallContainer}>
+          <label className={styles.label}>Pick up date</label>
+          <input type="date" id="date" name="date" className={styles.input} />
+        </div>
+        <div className={styles.smallContainer}>
+          <label className={styles.label}>Drop off date</label>
+          <input className={styles.input} type="date" id="ddate" name="ddate" />
+        </div>
+        <div className={styles.smallContainer}>
+          <label className={styles.label}>Pick vehicle brand</label>
+          <select className={styles.input} name="cars" id="cars">
+            <option value="" key="1">
+              Pick a vehicle
+            </option>
+            <option value="" key="2">
+              Land rover
+            </option>
+            <option value="" key="3">
+              Jeep
+            </option>
+            <option value="" key="4">
+              Ford
+            </option>
+            <option value="" key="5">
+              Toyota
+            </option>
+          </select>
+        </div>
+        <div className={styles.smallContainer}>
+          <button onClick={handleSubmit} className={styles.button}>
+            Find vehicle
+          </button>
+        </div>
       </div>
     </div>
   );
